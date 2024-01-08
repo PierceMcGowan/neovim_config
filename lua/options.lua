8f1b9f801850a362d9ebf42a5fc21bab8a605ec4
Pierce McGowan <piercemcgowan1998@gmail.com>
2024-01-07 19:57:10 -0500

    initial commit

---
+++ lua/options.lua
@@ -0,0 +1,26 @@
+--print("Options")
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.wrap = false
+
+vim.optexpandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+
+--system clipboard
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.termguicolors = true
+
+vim.g.mapleader = " "
